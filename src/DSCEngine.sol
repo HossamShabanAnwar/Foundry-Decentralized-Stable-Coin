5acd58f8fe0f162d65a0720b7d514a9d973f94ed
HossamShabanAnwar <hossamshabananwar@yahoo.com>
2024-09-15 16:24:07 +0300

    along the way..

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -67,7 +67,7 @@ contract DSCEngine is ReentrancyGuard {
     uint256 private constant PRECISION = 1e18; // To transform between ETH and WEI
     uint256 private constant LIQUIDATION_THRESHOLD = 50; // 200% overcollateralized
     uint256 private constant LIQUIDATION_PRECISION = 100;
-    uint256 private constant MIN_HEALTH_FACTOR = 1;
+    uint256 private constant MIN_HEALTH_FACTOR = 1e18;
 
     // In case of wrapped tokens (wBTC, WETH, ..) they are deployed as smart contracts so they have addresses
     mapping(address token => address priceFeed) private s_priceFeeds;
@@ -80,6 +80,7 @@ contract DSCEngine is ReentrancyGuard {
     /////////////////////////
 
     event CollateralDeposited(address indexed user, address indexed token, uint256 indexed amount);
+    event CollateralRedeemed(address indexed user, address indexed token, uint256 indexed amount);
 
     /////////////////////////
     /////// Modifiers ///////
@@ -111,15 +112,29 @@ contract DSCEngine is ReentrancyGuard {
         i_dsc = DecentralizedStableCoin(dscAddress);
     }
 
-    function depositCollateralAndMintDSC() external {}
+    /**
+     *
+     * @param tokenCollateralAddress The address of the token to deposit as collateral.
+     * @param amountCollateral The amount of collateral to deposit.
+     * @param amountDSCToMint The amount of decentralized stablecoin to mint.
+     * @notice This function will deposit your collateral and mint DSC in one transaction.
+     */
+    function depositCollateralAndMintDSC(
+        address tokenCollateralAddress,
+        uint256 amountCollateral,
+        uint256 amountDSCToMint
+    ) external {
+        depositCollateral(tokenCollateralAddress, amountCollateral);
+        mintDSC(amountDSCToMint);
+    }
 
     /**
-     * @notice Follows CEI
+     * @notice Follows CEI: Checks, Effects, Interactions
      * @param tokenCollateralAddress The address of the token to deposit as collateral.
      * @param amountCollateral The amount of collateral to deposit.
      */
     function depositCollateral(address tokenCollateralAddress, uint256 amountCollateral)
-        external
+        public
         moreThanZero(amountCollateral)
         isAllowedToken(tokenCollateralAddress)
         nonReentrant
@@ -132,16 +147,47 @@ contract DSCEngine is ReentrancyGuard {
         }
     }
 
-    function redeemCollateralForDSC() external {}
-
-    function redeemCollateral() external {}
+    /**
+     * @notice This function handles both redeeming collateral and burning minted DSC.
+     * @param tokenCollateralAddress The address of the token to deposit as collateral.
+     * @param amountCollateral The amount of collateral to deposit.
+     * @param amountDSCToBurn The amount of DSC to burn.
+     */
+    function redeemCollateralForDSC(address tokenCollateralAddress, uint256 amountCollateral, uint256 amountDSCToBurn)
+        external
+    {
+        burnDSC(amountDSCToBurn);
+        redeemCollateral(tokenCollateralAddress, amountCollateral);
+    }
 
     /**
-     * @notice Follows CEI
-     * @param amountDSCToMint The amount of decentralized stablecoin to mint
-     * @notice User must have more collateral value than the minimum threshold
+     * @param tokenCollateralAddress The address of the token to deposit as collateral.
+     * @param amountCollateral The amount of collateral to deposit.
      */
-    function mintDSC(uint256 amountDSCToMint) external moreThanZero(amountDSCToMint) nonReentrant {
+    function redeemCollateral(address tokenCollateralAddress, uint256 amountCollateral)
+        public
+        moreThanZero(amountCollateral)
+        nonReentrant
+    {
+        // TODO: We should've checked if the amount is less than total collateral
+        // if (amountCollateral >= s_collateralDeposited[msg.sender][tokenCollateralAddress]) {
+        //     revert;
+        // }
+        s_collateralDeposited[msg.sender][tokenCollateralAddress] -= amountCollateral;
+        emit CollateralRedeemed(msg.sender, tokenCollateralAddress, amountCollateral);
+        bool success = IERC20(tokenCollateralAddress).transfer(msg.sender, amountCollateral);
+        if (!success) {
+            revert DSCEngine__TransferFailed();
+        }
+        _revertIfHealthFactorIsBroken(msg.sender);
+    }
+
+    /**
+     * @notice Follows CEI: Checks, Effects, Interactions
+     * @param amountDSCToMint The amount of decentralized stablecoin to mint.
+     * @notice User must have more collateral value than the minimum threshold.
+     */
+    function mintDSC(uint256 amountDSCToMint) public moreThanZero(amountDSCToMint) nonReentrant {
         s_DSCMinted[msg.sender] += amountDSCToMint;
         // TODO: Why don't we emit an event here??
         _revertIfHealthFactorIsBroken(msg.sender);
@@ -151,11 +197,35 @@ contract DSCEngine is ReentrancyGuard {
         }
     }
 
-    function burnDSC() external {}
+    function burnDSC(uint256 amount) public moreThanZero(amount) {
+        s_DSCMinted[msg.sender] -= amount;
+        bool success = i_dsc.transferFrom(msg.sender, address(this), amount);
+        // This next check isn't probably gonna be called, cause the revert mechanism in transferFrom() will be called first.
+        // We put the check incase the transferFrom() revert wasn't triggered and the issue was somewhere else.
+        if (!success) {
+            revert DSCEngine__TransferFailed();
+        }
+        i_dsc.burn(amount);
+        _revertIfHealthFactorIsBroken(msg.sender); // This too probably will never be invoked..
+    }
 
     function getHealthFactor() external {}
 
-    function liquidate() external {}
+    /**
+     *
+     * @param collateral The ERC20 token address of the collateral you're using to make the protocol solvent again.
+     * This is collateral that you're going to take from the user who is insolvent.
+     * In return, you have to burn your DSC to pay off their debt, but you don't pay off your own.
+     * @param user The user who is insolvent. They have to have a _healthFactor below MIN_HEALTH_FACTOR.
+     * @param deptToCover The amount of DSC you want to burn to cover the user's debt.
+     *
+     * @notice You can partially liquidate a user.
+     * @notice You will get a 10% LIQUIDATION_BONUS for taking the users funds.
+     * @notice This function working assumes that the protocol will be roughly 150% over collateralized in order for this to work.
+     * @notice A known bug would be if the protocol was only 100% collateralized, we wouldn't be able to liquidate anyone.
+     * For example: if the price of the collateral plummeted before anyone could be liquidated.
+     */
+    function liquidate(address collateral, address user, uint256 deptToCover) external {}
 
     /////////////////////////////////////////
     /// Private & Internal View Functions ///
--- test/unit/DSCEngineTest.t.sol
+++ test/unit/DSCEngineTest.t.sol
@@ -6,6 +6,7 @@ import {DeployDSC} from "../../script/DepolyDSC.s.sol";
 import {HelperConfig} from "../../script/HelperConfig.s.sol";
 import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
 import {DSCEngine} from "../../src/DSCEngine.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
 
 contract DSCEngineTest is Test {
     DeployDSC deployer;
@@ -17,10 +18,15 @@ contract DSCEngineTest is Test {
     address wETH;
     address wBTC;
 
+    address public USER = makeAddr("user");
+    uint256 public constant AMOUNT_COLLATERAL = 10 ether;
+    uint256 public constant STARTING_ERC20_BALANCE = 10 ether;
+
     function setUp() public {
         deployer = new DeployDSC();
         (dsc, engine, config) = deployer.run();
         (wETHUSDPriceFeed,, wETH,,) = config.activeNetworkConfig();
+        ERC20Mock(wETH).mint(USER, STARTING_ERC20_BALANCE);
     }
 
     function testGetUSDValue() public view {
@@ -32,5 +38,13 @@ contract DSCEngineTest is Test {
 
         // Act / Assert
         assert(actualUSD == expectedUSD);
+    }
+
+    function testRevertIfCollateralEqualZero() public {
+        vm.startPrank(USER);
+        ERC20Mock(wETH).approve(address(engine), AMOUNT_COLLATERAL);
+        vm.expectRevert(DSCEngine.DSCEngine__AmountLessThanZero.selector);
+        engine.depositCollateral(wETH, 0);
+        vm.stopPrank();
     }
 }
